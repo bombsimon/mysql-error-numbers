4aaad51ca4350ec253c70afda40458fb237b2616
Simon Sawert <simon@sawert.se>
2019-11-08 09:34:56 +0100

    Don't fallback in FromError, add FromErrorOrString, update deps

--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/bombsimon/mysql-error-numbers
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.0
+	github.com/andybalholm/cascadia v1.1.0 // indirect
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/iancoleman/strcase v0.0.0-20190422225806-e506e3ef7365
-	golang.org/x/net v0.0.0-20190909003024-a7b16738d86b // indirect
-	google.golang.org/appengine v1.6.2 // indirect
+	golang.org/x/net v0.0.0-20191108063844-7e6e90b9ea88 // indirect
+	google.golang.org/appengine v1.6.5 // indirect
 )
+
+go 1.13
--- mysql_error_numbers_test.go
+++ mysql_error_numbers_test.go
@@ -1,6 +1,7 @@
 package mysqlerrnum
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/go-sql-driver/mysql"
@@ -9,7 +10,7 @@ import (
 func TestFromError(t *testing.T) {
 	cases := []struct {
 		name               string
-		err                *mysql.MySQLError
+		err                error
 		expects            ErrorNumber
 		expectsString      string
 		expectsDescription string
@@ -31,6 +32,13 @@ func TestFromError(t *testing.T) {
 			expects:            ErrNoReferencedRow,
 			expectsString:      "ER_NO_REFERENCED_ROW",
 			expectsDescription: "InnoDB reports this error when you try to add a row but there is no parent row, and a foreign key constraint fails. Add the parent row first.",
+		},
+		{
+			name:               "Should not fallback to string",
+			err:                errors.New("Error 1216: This should not be catched"),
+			expects:            ErrUnknownMySQLError,
+			expectsString:      "ER_UNKNOWN_MYSQL_ERROR",
+			expectsDescription: "Unknown MySQL error",
 		},
 	}
 
@@ -80,6 +88,65 @@ func TestFromString(t *testing.T) {
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
 			r := FromString(tc.errString)
+
+			if r != tc.expects {
+				t.Fatal("Expected other number")
+			}
+
+			if r.String() != tc.expectsString {
+				t.Fatal("Expected other string")
+			}
+
+			if r.Description() != tc.expectsDescription {
+				t.Fatal("Expected other description")
+			}
+		})
+	}
+}
+
+func TestFromErrorOrString(t *testing.T) {
+	cases := []struct {
+		name               string
+		err                error
+		expects            ErrorNumber
+		expectsString      string
+		expectsDescription string
+	}{
+		{
+			name:               "nil should not panic",
+			err:                nil,
+			expects:            ErrUnknownMySQLError,
+			expectsString:      "ER_UNKNOWN_MYSQL_ERROR",
+			expectsDescription: "Unknown MySQL error",
+		},
+		{
+			name:               "non MySQL error returns unknown",
+			err:                errors.New("this is not MySQL"),
+			expects:            ErrUnknownMySQLError,
+			expectsString:      "ER_UNKNOWN_MYSQL_ERROR",
+			expectsDescription: "Unknown MySQL error",
+		},
+		{
+			name: "Should succeed with first error",
+			err: &mysql.MySQLError{
+				Number: 1216,
+			},
+			expects:            ErrNoReferencedRow,
+			expectsString:      "ER_NO_REFERENCED_ROW",
+			expectsDescription: "InnoDB reports this error when you try to add a row but there is no parent row, and a foreign key constraint fails. Add the parent row first.",
+		},
+		{
+			name:               "Should succeed with error containing the string",
+			err:                errors.New("Error 1216: This should be found"),
+			expects:            ErrNoReferencedRow,
+			expectsString:      "ER_NO_REFERENCED_ROW",
+			expectsDescription: "InnoDB reports this error when you try to add a row but there is no parent row, and a foreign key constraint fails. Add the parent row first.",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			r := FromErrorOrString(tc.err)
 
 			if r != tc.expects {
 				t.Fatal("Expected other number")
